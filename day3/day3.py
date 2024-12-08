e429e83ce7312350a0ad1e30c1d37a06a93b6b93
lel <daniil.tsimbalyuk@gmail.com>
2024-12-07 16:05:28 -0800

    took 15 minutes for part1 and way too long for part2
    Had a dumb bug that took me so long
    
    I split at don't() and then I just matched everything after do() but that gave a super high answer, when I matched before do() and subtract it from part1 it worked, but it should have been the same I assume

---
+++ day3/day3.py
@@ -0,0 +1,13 @@
+import re
+
+pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+
+part1, part2 = 0, 0
+with open("input.txt", "r") as f:
+    text = f.read()
+    part1 = sum([int(a) * int(b) for a, b in pattern.findall(text)])
+    for invalid in ("do()" + text).split("don't()"):
+        part2 += sum([int(a) * int(b) for a, b in pattern.findall(invalid[:invalid.find("do()")])])
+
+print(f"Part1: {part1}")
+print(f"Part2: {part1 - part2}")
